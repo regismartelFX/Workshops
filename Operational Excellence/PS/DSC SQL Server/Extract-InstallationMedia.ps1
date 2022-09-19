09da8066793a36817d62604bde439be8afd141bb
Régis Martel <regis.martel@fxinnovation.com>
2022-09-19 16:36:08 -0400

    install SQL Server with DSC

---
+++ Operational Excellence/PS/DSC SQL Server/Extract-InstallationMedia.ps1
@@ -0,0 +1,10 @@
+﻿#https://learn.microsoft.com/en-us/sql/database-engine/install-windows/install-sql-server-with-powershell-desired-state-configuration?view=sql-server-ver16#get-the-sql-server-2017-installation-media
+$ImagePath = 'C:\Sources\SW_DVD9_NTRL_SQL_Svr_Standard_Edtn_2019Dec2019_64Bit_English_OEM_VL_X22-22109.iso'
+$Destination = 'C:\Sources\SQL2019\'
+
+New-Item -Path $Destination -ItemType 'Directory' -ErrorAction 'SilentlyContinue'
+$DiskImage = Mount-DiskImage -ImagePath $ImagePath -PassThru
+$Volume = $DiskImage | Get-Volume
+$PSDrive = Get-PSDrive -Name $Volume.DriveLetter
+Copy-Item -Path (Join-Path -Path $PSDrive.Root -ChildPath '*') -Destination $Destination -Recurse
+Dismount-DiskImage -ImagePath $ImagePath
