329c83b27a6d8de4ee8a53f011fdb0e43ac6acdf
Régis Martel <regis.martel@fxinnovation.com>
2022-09-26 17:03:22 -0400

    refactor data azurerm_subscription

--- Operational Excellence/TF/Global/Core/main.tf
+++ Operational Excellence/TF/Global/Core/main.tf
@@ -1,5 +1,5 @@
 data "azurerm_subscription" "sandbox" {
-  subscription_id = "88fa2133-05ce-4034-b30f-5a47e7240156"
+  provider = azurerm.sandbox
 }
 
 
@@ -22,14 +22,14 @@ resource "azurerm_key_vault" "core" {
   resource_group_name             = azurerm_resource_group.core.name
   enabled_for_disk_encryption     = true
   enabled_for_template_deployment = true
-  tenant_id                       = data.azurerm_client_config.current.tenant_id
+  tenant_id                       = data.azurerm_subscription.sandbox.tenant_id
   soft_delete_retention_days      = 7
   purge_protection_enabled        = false
 
   sku_name = "standard"
 
   access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
+    tenant_id = data.azurerm_subscription.sandbox.tenant_id
     object_id = data.azurerm_client_config.current.object_id
 
     key_permissions = [
@@ -85,7 +85,7 @@ resource "azurerm_virtual_network" "core" {
 }
 
 
-resource "azurerm_subnet" "vmsubnet" {
+resource "azurerm_subnet" "core" {
   provider = azurerm.sandbox
 
   name                                      = "snet-${module.info.descriptive_context}"
