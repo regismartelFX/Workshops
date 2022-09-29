925b4fb7ad8e10741492087078c722f6229d4787
Régis Martel <regis.martel@fxinnovation.com>
2022-09-29 09:47:07 -0400

    add dsc configuration SQLInstall to automation account

--- Operational Excellence/Global/VM/dsc_configuration.tf
+++ Operational Excellence/Global/VM/dsc_configuration.tf
@@ -1,46 +1,38 @@
-resource "azurerm_automation_dsc_configuration" "sql" {
+#Does not work very well.  Use the manual method in Operational Excellence/Global/VM/How to install SQL Server with Azure Automation State Configuration.md
+#For this configuration to work (theorically), you need to compile SQLInstall.ps1 locally to create the .mof file.
+
+resource "azurerm_automation_module" "sqlserverdsc" {
+  provider = azurerm.sandbox
+  count = module.info.dsc_sqlinstall
+     
+  name                    = "SqlServerDsc"
+  resource_group_name     = data.terraform_remote_state.core.outputs.core_resource_group_name
+  automation_account_name = data.terraform_remote_state.core.outputs.core_automation_account_name
+
+  module_link {
+    uri = "https://www.powershellgallery.com/api/v2/package/SqlServerDsc/16.0.0"
+  }
+}
+
+resource "azurerm_automation_dsc_configuration" "sqlinstall" {
+  provider = azurerm.sandbox
+  count = module.info.dsc_sqlinstall  
+
   name                    = "SQLInstall"
   resource_group_name     = data.terraform_remote_state.core.outputs.core_resource_group_name
   automation_account_name = data.terraform_remote_state.core.outputs.core_automation_account_name
   location                = module.info.primary_region.name
-  content_embedded        = <<CONTENT_EMBEDDED
-Configuration SQLInstall
-{
-     Import-DscResource -ModuleName 'SqlServerDsc'
-     Import-DscResource –ModuleName 'PSDesiredStateConfiguration'
-
-     node localhost
-     {
-          WindowsFeature 'NetFramework45'
-          {
-               Name   = 'NET-Framework-45-Core'
-               Ensure = 'Present'
-          }
-
-          SqlSetup 'InstallDefaultInstance'
-          {
-               InstanceName        = 'MSSQLSERVER'
-               Features            = 'SQLENGINE'
-               SourcePath          = 'C:\Sources\SQL2019\'
-               SQLSysAdminAccounts = @('Administrators')
-               DependsOn           = '[WindowsFeature]NetFramework45'
-          }
-
-          Package 'SSMS' {
-               Ensure = 'Present'
-               Name = "SSMS-Setup-ENU"
-               Path = 'C:\Sources\SSMS\SSMS-Setup-ENU.exe'
-               Arguments = '/install /passive /norestart'
-               ProductId ='ECC23FD6-535B-43CB-894B-F47FA605EBB3' # SQL Server Management Studio 15.0.18424.0
-          }
-     }
-}
-  CONTENT_EMBEDDED
+  content_embedded        = "Configuration SQLInstall {}"
 }
 
+resource "azurerm_automation_dsc_nodeconfiguration" "sqlinstall" {
+  provider = azurerm.sandbox
+  count = module.info.dsc_sqlinstall  
+     
+  name                    = "SQLInstall.localhost"
+  resource_group_name     = data.terraform_remote_state.core.outputs.core_resource_group_name
+  automation_account_name = data.terraform_remote_state.core.outputs.core_automation_account_name
+  content_embedded        = "${file("${path.cwd}/sqlinstall/localhost.mof")}"
 
-
-
-
-
-
+  depends_on              = [azurerm_automation_dsc_configuration.sqlinstall]
+}
